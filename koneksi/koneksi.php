ac3ccabb1f1e857524cb7726ed590355e11a43fe
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-01-14 14:27:01 +0700

    add database in repo

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $username = "root";
-    $password = "gokpras123";
+    $password = "";
     $db_name = "label_example";
 
     $koneksi = mysqli_connect($host, $username, $password, $db_name) or trigger_error(mysqli_error($koneksi), E_USER_NOTICE);
